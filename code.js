a2289ecd32cd3da0b29613f527559e64abdd04b9
brekkenjackson <106278680+brekkenjackson@users.noreply.github.com>
2022-06-13 15:08:19 -0500

    completed exercise 2, returns as a string

--- code.js
+++ code.js
@@ -7,13 +7,14 @@
 const helloWorld = function () {
     return "Hello, World!";
 }
+// function fooFoo() {
+//     return 2;
+// }
 
+// 1.
 const sayHello = function () {
-
+    return " "
 }
 
-function fooFoo() {
-    return 2;
-}
-
+// 2.
 
--- tests.js
+++ tests.js
@@ -15,17 +15,23 @@ describe('helloWorld', function() {
 });
 
 
-describe("fooFoo", function () {
-    it('should return true if foo() === 2', function () {
-        let expectedResult = 2;
-        let actualResult = fooFoo();
-        expect(actualResult).toBe(expectedResult);
-    })
-})
+// describe("fooFoo", function () {
+//     it('should return true if foo() === 2', function () {
+//         let expectedResult = 2;
+//         let actualResult = fooFoo();
+//         expect(actualResult).toBe(expectedResult);
+//     })
+// })
 
+// 1.
 
 describe('sayHello', function() {
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
+    })
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe('string')
     });
 });
+
+// 2.
